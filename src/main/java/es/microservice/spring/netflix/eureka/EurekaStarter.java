e13eb2bc03f44d8916f767ce5c59136c2a85dc0a
Jorge Valls <jvalls@paradigmadigital.com>
2017-11-02 19:31:00 +0100

    Eureka starter

--- src/main/java/es/microservice/spring/netflix/eureka/EurekaStarter.java
+++ src/main/java/es/microservice/spring/netflix/eureka/EurekaStarter.java
@@ -1,3 +1,5 @@
+package es.microservice.spring.netflix.eureka;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
@@ -5,6 +7,11 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @EnableEurekaServer
 @SpringBootApplication
 public class EurekaStarter {
+	
+	public boolean testMethod() {
+		System.out.println("TEST");
+		return true;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaStarter.class, args);
